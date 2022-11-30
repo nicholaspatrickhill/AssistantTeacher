addfb0fc122769870124951ad7b766e7b2b182f0
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-29 21:13:07 -0500

    added GetClassName

---
+++ AssistantTeacher/Menu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AssistantTeacher
+{
+    public class Menu
+    {
+        private int SelectedIndex;
+        private readonly string[] Options;
+        private readonly string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+            SelectedIndex = 0;
+        }
+
+        private void DisplayOptions()
+        {
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+
+                if (i == SelectedIndex)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+
+                WriteLine(currentOption);
+            }
+            ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex--;
+                    if (SelectedIndex == -1)
+                    {
+                        SelectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex++;
+                    if (SelectedIndex == Options.Length)
+                    {
+                        SelectedIndex = 0;
+                    }
+                }
+
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return SelectedIndex;
+        }
+    }
+}
+
+
+
--- AssistantTeacher/Program.cs
+++ AssistantTeacher/Program.cs
@@ -6,11 +6,67 @@ namespace AssistantTeacher
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello, Teach! How many students are in your class?");
+            Title = "AssistantTeacher";
+            WriteLine("Hello, Teach!");
+            Thread.Sleep(500);
+            WriteLine("I'll be your assistant today.");
+            Thread.Sleep(500);
+            GetClassName();
+        }
+
+        private static void GetClassName()
+        {
+            WriteLine("To get started, please tell me the name of the class you're teaching: ");
+
+            string? className = ReadLine();
+
+            Thread.Sleep(500);
+            WriteLine("\nGreat! You're teaching " + className + " is that correct?");
+            WriteLine("Type y and press enter to confirm. Or type n and press enter to start over.");
+
+            string? confirmClassName = ReadLine().ToLower();
+
+            if (confirmClassName == "y")
+            {
+                WriteLine("Awesome. Now please tell me the total number of students in your class: ");
+                CreateStudentList();
+            }
+            else if (confirmClassName == "n")
+            {
+                WriteLine("Oh no! Let's start over.");
+            }
+            else
+            {
+                WriteLine("I'm sorry, I don't recognize that input. Please try again.");
+            }
+        }
+
+
+
+        private static void CreateStudentList()
+        {
+            WriteLine("Please tell me the total number of students in your class: ");
 
             int studentNum = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nYou have " + studentNum + " students! Now please enter their names: ");
+            Thread.Sleep(500);
+            WriteLine("\nYou have " + studentNum + " students! Is that correct?");
+            WriteLine("Press y and press enter to confirm. Or type n and press enter to start over");
+
+            string? confimStudentNum = ReadLine().ToLower();
+
+            if (confimStudentNum == "y")
+            {
+                WriteLine("Great! Enter Their names below: ");
+            }
+            else if (confimStudentNum == "n")
+            {
+                WriteLine("Oh No! Let's start over.");
+            }
+            else
+            {
+                WriteLine("Invalid input. Please try again.");
+            }
 
             string[] studentNames = new string[studentNum];
 
@@ -22,12 +78,12 @@ namespace AssistantTeacher
             Array.Sort(studentNames);
 
             Console.WriteLine("\nHere is an alphabatized list of your students:");
-            
+
             for (int i = 0; i < studentNames.Length; i++)
             {
                 WriteLine(studentNames[i]);
             }
-
         }
+  
     }
 }
