dac3b4cd34d17e7cc3fa7c14e6bfb8c2f9df3730
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-29 21:19:13 -0500

    refactored, created basic logic for getting class, number and names

--- AssistantTeacher/Program.cs
+++ AssistantTeacher/Program.cs
@@ -24,26 +24,31 @@ namespace AssistantTeacher
             WriteLine("\nGreat! You're teaching " + className + " is that correct?");
             WriteLine("Type y and press enter to confirm. Or type n and press enter to start over.");
 
+            ConfirmClassName();
+        }
+
+        private static void ConfirmClassName()
+        {
             string? confirmClassName = ReadLine().ToLower();
 
             if (confirmClassName == "y")
             {
                 WriteLine("Awesome. Now please tell me the total number of students in your class: ");
-                CreateStudentList();
+                GetTotalNumberOfStudents();
             }
             else if (confirmClassName == "n")
             {
                 WriteLine("Oh no! Let's start over.");
+                GetClassName();
             }
             else
             {
                 WriteLine("I'm sorry, I don't recognize that input. Please try again.");
+                ConfirmClassName();
             }
         }
 
-
-
-        private static void CreateStudentList()
+        private static void GetTotalNumberOfStudents()
         {
             WriteLine("Please tell me the total number of students in your class: ");
 
@@ -52,22 +57,32 @@ namespace AssistantTeacher
             Thread.Sleep(500);
             WriteLine("\nYou have " + studentNum + " students! Is that correct?");
             WriteLine("Press y and press enter to confirm. Or type n and press enter to start over");
+            ConfirmStudentNum(studentNum);
+        }
 
+        private static void ConfirmStudentNum(int studentNum)
+        {
             string? confimStudentNum = ReadLine().ToLower();
 
             if (confimStudentNum == "y")
             {
                 WriteLine("Great! Enter Their names below: ");
+                CreateStudentList(studentNum);
             }
             else if (confimStudentNum == "n")
             {
                 WriteLine("Oh No! Let's start over.");
+                GetTotalNumberOfStudents();
             }
             else
             {
-                WriteLine("Invalid input. Please try again.");
-            }
+                WriteLine("I'm sorry, I don't recognize that input. Please try again.");
+                ConfirmStudentNum(studentNum);
+            }  
+        }
 
+        private static void CreateStudentList(int studentNum)
+        {
             string[] studentNames = new string[studentNum];
 
             for (int i = 0; i < studentNames.Length; i++)
@@ -84,6 +99,5 @@ namespace AssistantTeacher
                 WriteLine(studentNames[i]);
             }
         }
-  
     }
 }
